a06dd6c8ac1d22c3118fea3749af041ee333be81
Kevin Cuzner <kevin@kevincuzner.com>
2015-12-29 22:21:20 -0700

    Moved dispatcher into asm volatile
    It actually seems to work better...
    
    I also discovered the sbiw instruction which is awesome

--- src/kos.c
+++ src/kos.c
@@ -207,3 +207,104 @@ void kos_schedule(void)
     }
 }
 
+void kos_dispatch(KOS_Task *task)
+{
+    // the call to this function should push the return address into the stack.
+    // we will now construct saving context. The entire context needs to be
+    // saved because it is very possible that this could be called from within
+    // an isr that doesn't use the call-used registers and therefore doesn't
+    // save them.
+    asm volatile (
+            "push r31 \n\t"
+            "push r30 \n\t"
+            "push r29 \n\t"
+            "push r28 \n\t"
+            "push r27 \n\t"
+            "push r26 \n\t"
+            "push r25 \n\t"
+            "push r24 \n\t"
+            "push r23 \n\t"
+            "push r22 \n\t"
+            "push r21 \n\t"
+            "push r20 \n\t"
+            "push r19 \n\t"
+            "push r18 \n\t"
+            "push r17 \n\t"
+            "push r16 \n\t"
+            "push r15 \n\t"
+            "push r14 \n\t"
+            "push r13 \n\t"
+            "push r12 \n\t"
+            "push r11 \n\t"
+            "push r10 \n\t"
+            "push r9 \n\t"
+            "push r8 \n\t"
+            "push r7 \n\t"
+            "push r6 \n\t"
+            "push r5 \n\t"
+            "push r4 \n\t"
+            "push r3 \n\t"
+            "push r2 \n\t"
+            "push r1 \n\t"
+            "push r0 \n\t"
+            "in   r0, %[_SREG_] \n\t" //push sreg
+            "push r0 \n\t"
+            "lds  r26, kos_current_task \n\t"
+            "lds  r27, kos_current_task+1 \n\t"
+            "sbiw r26, 0 \n\t"
+            "breq 1f \n\t" //null check, skip next section
+            "in   r0, %[_SPL_] \n\t"
+            "st   X+, r0 \n\t"
+            "in   r0, %[_SPH_] \n\t"
+            "st   X+, r0 \n\t"
+            "1:" //begin dispatching
+            "mov  r26, %A[_next_task_] \n\t"
+            "mov  r27, %B[_next_task_] \n\t"
+            "sts  kos_current_task, r26 \n\t" //set current task
+            "sts  kos_current_task+1, r27 \n\t"
+            "ld   r0, X+ \n\t" //load stack pointer
+            "out  %[_SPL_], r0 \n\t"
+            "ld   r0, X+ \n\t"
+            "out  %[_SPH_], r0 \n\t"
+            "pop  r0 \n\t"
+            "out  %[_SREG_], r0 \n\t"
+            "pop  r0 \n\t"
+            "pop  r1 \n\t"
+            "pop  r2 \n\t"
+            "pop  r3 \n\t"
+            "pop  r4 \n\t"
+            "pop  r5 \n\t"
+            "pop  r6 \n\t"
+            "pop  r7 \n\t"
+            "pop  r8 \n\t"
+            "pop  r9 \n\t"
+            "pop  r10 \n\t"
+            "pop  r11 \n\t"
+            "pop  r12 \n\t"
+            "pop  r13 \n\t"
+            "pop  r14 \n\t"
+            "pop  r15 \n\t"
+            "pop  r16 \n\t"
+            "pop  r17 \n\t"
+            "pop  r18 \n\t"
+            "pop  r19 \n\t"
+            "pop  r20 \n\t"
+            "pop  r21 \n\t"
+            "pop  r22 \n\t"
+            "pop  r23 \n\t"
+            "pop  r24 \n\t"
+            "pop  r25 \n\t"
+            "pop  r26 \n\t"
+            "pop  r27 \n\t"
+            "pop  r28 \n\t"
+            "pop  r29 \n\t"
+            "pop  r30 \n\t"
+            "pop  r31 \n\t"
+            "ret \n\t"
+            "" ::
+            [_SREG_] "i" _SFR_IO_ADDR(SREG),
+            [_SPL_] "i" _SFR_IO_ADDR(SPL),
+            [_SPH_] "i" _SFR_IO_ADDR(SPH),
+            [_next_task_] "r" (task));
+}
+
